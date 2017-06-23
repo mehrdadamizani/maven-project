fc6c6920d8bcd2a874cbeff7adfd1b707ab9651b
Mehrdad Mizani <mehrdadmizani@gmail.com>
2017-06-23 12:38:32 +0300

    fixed the problem with javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,8 @@ public class Greeter {
 
   /**
    *
-   * @param someone
-   * @return something
+   * @param someone the name
+   * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
